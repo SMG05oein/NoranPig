78bdc047c4f25dd08f91883b01036d3c46b52aee
SMG <SMG05eoin@gmial.com>
2024-12-09 10:25:24 +0900

    커밋 메세지 쓰기 귀찮아요

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 // Render 환경에서 포트 설정
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware 설정
 app.use(cors());
